082e3aa92eed8d8a4cd4c6d775994da1018c0abf
cooooooolethanhackerz22113 <wangethanhk@gmail.com>
2024-01-06 12:01:18 +0800

    hi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Q1: 
 '''
 
-x = None    # TODO: Replace `None` with your answer
+x = 10   # TODO: Replace `None` with your answer
 
 print('Q1:')
 print(f'x = {x}')
@@ -13,9 +13,9 @@ print('#' * 30)
 Q2:
 '''
 
-a = None    # TODO: Replace `None` with your answer
-b = None    # TODO: Replace `None` with your answer
-c = None    # TODO: Replace `None` with your answer
+a = 6    # TODO: Replace `None` with your answer
+b = 9    # TODO: Replace `None` with your answer
+c = 16    # TODO: Replace `None` with your answer
 
 print('Q2:')
 print(f'a = {a}')
@@ -27,8 +27,8 @@ print('#' * 30)
 Q3:
 '''
 
-first = None    # TODO: Replace `None` with your answer
-second = None   # TODO: Replace `None` with your answer
+first = 19    # TODO: Replace `None` with your answer
+second = 8   # TODO: Replace `None` with your answer
 
 print('Q3:')
 print(f'first = {first}')
@@ -40,19 +40,51 @@ Q4:
 '''
 
 # TODO: Write your code here
+first = 8
+second = 19
+first += second 
+second = first - second 
+first -= second
 
+print(first, second)
 ##################################################
 '''
 Q5:
 '''
 
 # TODO: Write your code here
-
+work_hours = 4 + 5 + 8 + 4
+hourly_salary = 8.75
+taxes = 0.20
+# Calculate pay at work based on hours worked each day
+print("My total hours worked:")
+print(work_hours)
+print("My hourly salary:")
+print("$8.75")
+print("My total pay:")
+print((work_hours) * hourly_salary)
+print("My taxes owed:")  # 20% tax
+print((work_hours) * hourly_salary * taxes)
 ##################################################
 '''
 Q6:
 '''
 
 # TODO: Write your code here
-
+Subtotal = 38 + 40 + 30
+Tax = 0.08
+Tip = 0.15
+# This program computes the total amount owed for a meal,
+# assuming 8% tax and a 15% tip.
+print("Subtotal:")
+print(Subtotal)
+    
+print("Tax:")
+print((Subtotal) * Tax)
+    
+print("Tip:")
+print((Subtotal) * Tip)
+    
+print("Total:")
+print(Subtotal + (Subtotal) * Tax + (Subtotal) * Tip)
 ##################################################
